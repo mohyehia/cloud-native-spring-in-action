a0bbc03460e02d9b95af47dbe8a6e0d337a15b64
mohyehia <mohammedyehia99@gmail.com>
2026-01-21 20:41:39 +0200

    Initialize order service with Spring Boot setup and basic order retrieval endpoint

---
+++ order-service/src/test/java/com/moh/yehia/order/service/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.moh.yehia.order.service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
