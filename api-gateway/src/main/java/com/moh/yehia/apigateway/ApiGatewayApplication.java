c603f3a1e36d7915f662530d7b5870b160e45566
mohyehia <mohammedyehia99@gmail.com>
2026-02-23 03:05:02 +0200

    Add circuit breaker configuration and fallback endpoints for catalog and order services

--- api-gateway/src/main/java/com/moh/yehia/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/moh/yehia/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,13 @@ package com.moh.yehia.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +17,17 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+}
+
+@Configuration
+class FallbackEndpoints{
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .GET("/catalog-fallback", request -> ServerResponse.ok().body(Mono.just("catalog-service is not available at this moment!"), String.class))
+                .POST("/catalog-fallback", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
+                .GET("/order-fallback", request -> ServerResponse.ok().body(Mono.just("catalog-service is not available at this moment!"), String.class))
+                .POST("/order-fallback", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build())
+                .build();
+    }
 }
